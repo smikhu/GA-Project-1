14a91a25d0d5af816e4bc690b7730a4823036338
Steven Mikhu <StevenMikhu@gmail.com>
2022-01-05 10:14:13 -0500

    Got the card to flip through CSS and some JS

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log('JS linked')
+// variables
+
+let cards = document.querySelectorAll('.card')
+
+// a flip card function that toggles a flip on click
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+
+
+
+// add a moves decrement that decreases each time the player completes a flip cycle
+
+// let counter;
+// let moves = 6;
+
+// function moveCounter() {
+//     moves--;
+//     counter.innerHTML = moves;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
